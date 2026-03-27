dbd8c10c575732191db67dea1699f55d7e0bc8ec
Minh <nguyenthiminh22122004@gmail.com>
2026-03-27 10:31:34 +0700

    feat: update App entry point with navigation and database init

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { RootNavigator } from './src/navigation/RootNavigator';
+import { AppNavigator } from './src/navigation/AppNavigator';
+import { initDatabase } from './src/db/database';
 
 export default function App() {
+  React.useEffect(() => {
+    initDatabase();
+  }, []);
+
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <RootNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
